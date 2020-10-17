81f79e0040fe99ce3db8295cf17ade737ebf8074
Duong <duongtruongtrong@gmail.com>
2020-10-17 10:18:02 +0700

    initiate files for streamlit

---
+++ nyc_streamlit.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.filterwarnings('ignore')
+
+import streamlit as st
+
+# Outline:
+# 1. Airbnb instroduction
+# 2. Big question
+# 3. COVID - Why still airbnb?
+#     Unique places with unique experience;
+#     Time to relax a bit, but still follow COVID rules.
+# 4. How to choose an airbnb:
+#     Price ranges;
+#     Accommodate types;
+#     Locations;
+#     Unique experience;
+#     Cleanliness;
+#     Safety: Crime Rate, COVID Rate.
+# 5. Result: A short list of "unique" airbnb to choose
+# 6. Project difficulty
+
+if teacher_or_student != "Teacher":
+    folder_path + '
