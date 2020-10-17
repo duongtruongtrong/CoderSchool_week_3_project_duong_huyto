efef865442d196a28b23d424be493ddaece5ed2e
Wiito21012001 <62187379+Wiito21012001@users.noreply.github.com>
2020-10-17 10:22:59 +0700

    Update nyc_streamlit.py
    Airbnb introduction

--- nyc_streamlit.py
+++ nyc_streamlit.py
@@ -9,6 +9,38 @@ warnings.filterwarnings('ignore')
 
 import streamlit as st
 
+st.header('Airbnb Introduction:')
+st.write('- Airbnb, the world leader in accommodations of the “sharing economy”, allows you to find places to stay directly from individuals in thousands of cities around the world.')
+st.write('- It allows you to rent apartments (or even entire houses) from people all over the world, almost everywhere in fact. The platform really revolutionized the world of accommodations.')
+
+
+st.header('Covid - Why choose an 
+
+
+
+Airbnb?')
+
+st.write('We are restricted to travel ever since the pandemic started. Therefore, the best option we have is to travel around the our area')
+st.write('So why choose an Airbnb?')
+
+st.markdown('**Live As a Local**')
+st.write('- Airbnb provides you the unique experience that hotels cannot.')
+st.write('- Airbnb allows you to live more like a local than a tourist.')
+st.write('- Airbnb usually does a better job of immersing you in the local culture than getting a hotel room')
+
+
+st.markdown('**Meet People**')
+st.write('- Airbnb gives you the option to stay with the host. The host will often be happy to welcome you and to tell you every interesting thing about the place you visit.')
+
+st.markdown('**Save Money(sometimes)**')
+st.write('- Depends on the location, Airbnbs are usually cheaper than hotels')
+st.write('- Especially when you travel in a group, retinting an Airbnb is definitely cheaper than a hotel')
+
+st.markdown('**Unique Experience**')
+st.write('- Some Airbnbs blah blah')
+
+st.header('How To choose an Airbnb?')
+
 # Outline:
 # 1. Airbnb instroduction
 # 2. Big question
