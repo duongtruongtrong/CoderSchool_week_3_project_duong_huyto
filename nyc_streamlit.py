139cce0f806b5055dcf9482f7df5586e3a55204f
Duong <duongtruongtrong@gmail.com>
2020-10-17 10:37:22 +0700

    change wording

--- nyc_streamlit.py
+++ nyc_streamlit.py
@@ -9,16 +9,28 @@ warnings.filterwarnings('ignore')
 
 import streamlit as st
 
+# Outline:
+# 1. Airbnb instroduction
+# 2. Big question
+# 3. COVID - Why still airbnb?
+#     Unique places with unique experience;
+#     Time to relax a bit, but still follow COVID rules.
+# 4. How to choose an airbnb:
+#     Price ranges;
+#     Accommodate types;
+#     Locations;
+#     Unique experience;
+#     Cleanliness;
+#     Safety: Crime Rate, COVID Rate.
+# 5. Result: A short list of "unique" airbnb to choose
+# 6. Project difficulty
+
 st.header('Airbnb Introduction:')
 st.write('- Airbnb, the world leader in accommodations of the “sharing economy”, allows you to find places to stay directly from individuals in thousands of cities around the world.')
 st.write('- It allows you to rent apartments (or even entire houses) from people all over the world, almost everywhere in fact. The platform really revolutionized the world of accommodations.')
 
 
-st.header('Covid - Why choose an 
-
-
-
-Airbnb?')
+st.header('Covid - Why choose an Airbnb?')
 
 st.write('We are restricted to travel ever since the pandemic started. Therefore, the best option we have is to travel around the our area')
 st.write('So why choose an Airbnb?')
@@ -40,22 +52,3 @@ st.markdown('**Unique Experience**')
 st.write('- Some Airbnbs blah blah')
 
 st.header('How To choose an Airbnb?')
-
-# Outline:
-# 1. Airbnb instroduction
-# 2. Big question
-# 3. COVID - Why still airbnb?
-#     Unique places with unique experience;
-#     Time to relax a bit, but still follow COVID rules.
-# 4. How to choose an airbnb:
-#     Price ranges;
-#     Accommodate types;
-#     Locations;
-#     Unique experience;
-#     Cleanliness;
-#     Safety: Crime Rate, COVID Rate.
-# 5. Result: A short list of "unique" airbnb to choose
-# 6. Project difficulty
-
-if teacher_or_student != "Teacher":
-    folder_path + '
